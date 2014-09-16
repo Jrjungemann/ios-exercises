48fb08cc5577c8ca71d8afd04018ae9a9690de0b
Jonathan Jungemann <jrjungemann@gmail.com>
2014-09-16 18:42:41 -0500

    Actually the NSLog stuff

---
+++ Lesson-5-v2/Sandbox/Sandbox/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Sandbox
+//
+//  Created by Jon Jungemann on 9/16/14.
+//  Copyright (c) 2014 Bloc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
