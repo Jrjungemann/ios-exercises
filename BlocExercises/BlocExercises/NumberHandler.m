a95b00b5de2b085a014f568b70e00aa32bb4a6d4
Jonathan Jungemann <jrjungemann@gmail.com>
2014-09-26 10:29:05 -0500

    Completed simple data types and opjects

--- BlocExercises/BlocExercises/NumberHandler.m
+++ BlocExercises/BlocExercises/NumberHandler.m
@@ -11,18 +11,46 @@
 @implementation NumberHandler
 
 - (NSNumber *) numberThatIsTwiceAsBigAsNumber:(NSNumber *)number {
-    /* WORK HERE */
-    return @0;
+    
+    NSInteger unboxing = [number integerValue] * 2;
+    NSNumber *boxing = [NSNumber numberWithInteger:unboxing];
+
+    return boxing;
 }
 
 - (NSArray *) arrayOfNumbersBetweenNumber:(NSInteger)number andOtherNumber: (NSInteger)otherNumber {
-    /* WORK HERE */
-    return @[];
+    
+    NSMutableArray *arrayOfNumbers = [NSMutableArray new];
+    
+    if (number > otherNumber) {
+        NSInteger temp = otherNumber;
+        otherNumber = number;
+        number = temp;
+    }
+    
+    [arrayOfNumbers addObject:[NSNumber numberWithLong:number]];
+    
+    while (number < otherNumber) {
+        number++;
+        [arrayOfNumbers addObject:[NSNumber numberWithLong:number]];
+    }
+    return arrayOfNumbers;
 }
 
 - (NSInteger) lowestNumberInArray:(NSArray *)arrayOfNumbers {
-    /* WORK HERE */
-    return 0;
+   
+    NSNumber *lowestNumber = arrayOfNumbers[0];
+    
+    for (NSNumber* number in arrayOfNumbers) {
+//      number is the current object being called
+        if (number.integerValue < lowestNumber.integerValue) {
+//      you can only compare simple types with ==, <, etc....
+            lowestNumber = number;
+        }
+    }
+    
+    return lowestNumber.intValue;
+//    return [lowestNumber intValue] this way works too
 }
 
 @end
